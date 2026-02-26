463309af21ffd8c041570473d649ff482605fb0c
ft-frank <frank.imagination@gmail.com>
2026-02-26 14:33:53 +1100

    finishing the vercel course on next.js

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import '@/app/ui/global.css' //@ means the root i'm pretty sure.
+import { inter, lusitana } from '@/app/ui/fonts'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className = {`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
